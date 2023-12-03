27ecffb8759681932185cf8c49256efacdccda7b
Shanto Noor <shantonoor900@gmail.com>
2023-12-04 03:59:08 +0600

    MyAddedFoodItems page complete

--- src/pages/MyAddedFoodItems.jsx
+++ src/pages/MyAddedFoodItems.jsx
@@ -1,5 +1,112 @@
+import { useQuery } from "@tanstack/react-query";
+import { axiosn } from "../hooks/useAxios";
+import useAuth from "../hooks/useAuth";
+import Spinner from "../components/Spinner";
+import {
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  InputBase,
+  Pagination,
+  Paper,
+  Stack,
+} from "@mui/material";
+import RecipeReviewCard from "../components/RecipeReviewCard";
+import { useRef, useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
+
 const MyAddedFoodItems = () => {
-  return <div>MyAddedFoodItems</div>;
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const [search, setSearch] = useState("");
+  const serachRef = useRef(null);
+  const [page, setPage] = useState(1);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ["/foods", `created_by=${user._id}`],
+    queryFn: async () => {
+      try {
+        const res = await axiosn.get(`/foods?created_by=${user._id}`);
+        return res.data;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    enabled: !!user._id,
+  });
+
+  if (isPending) return <Spinner />;
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <Container sx={{ mt: 5 }}>
+      <Paper
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: 400,
+          mx: "auto",
+          mb: 5,
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Food"
+          inputRef={serachRef}
+          defaultValue={search}
+        />
+        <IconButton
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="search"
+          onClick={() => {
+            setSearch(serachRef.current.value);
+          }}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+
+      <Grid container gap={5} alignItems="stretch" justifyContent="center">
+        {data[0].map((food) => {
+          return (
+            <Grid item key={food._id} xs={12} md={4} lg={3}>
+              <RecipeReviewCard food={food}>
+                <Button
+                  onClick={() => navigate(`/single-food-item/${food._id}`)}
+                  size="large"
+                >
+                  Details
+                </Button>
+                <Button
+                  size="large"
+                  onClick={() => navigate(`/update-food-item/${food._id}`)}
+                >
+                  Update
+                </Button>
+              </RecipeReviewCard>
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Stack justifyContent="center" alignItems="center" mt={5}>
+        <Pagination
+          count={Math.ceil(data[1] / 9)}
+          color="primary"
+          page={page}
+          onChange={handleChangePage}
+        />
+      </Stack>
+    </Container>
+  );
 };
 
 export default MyAddedFoodItems;
